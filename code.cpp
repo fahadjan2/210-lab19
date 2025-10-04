c5c8c3588922fa30aa02495362ede05132fe8b07
fahadjan2 <fahadfawad2006@gmail.com>
2025-10-03 20:15:38 -0700

    Commit

--- code.cpp
+++ code.cpp
@@ -15,13 +15,41 @@ struct MovieReviews {
 class Movie {
     private: 
     string title;
-    Movie * head;
+    MovieReviews * head;
 
     public:
     Movie(string t) {title = t;}
-};
 
-void output(MovieReviews *);
+    //Adds a review
+    void addReview(double r, string c) {
+        MovieReviews * newReview = new MovieReviews;
+        if (!head) {
+            newReview->next = nullptr;
+        }
+        else {
+            newReview->next = head;
+        }
+        newReview->rating = r;
+        newReview->comment = c;
+        head = newReview;
+    }
+
+    //Getter for head
+    void getReviews() {
+        if (!head) {
+        cout << "Empty list" << endl;
+        return;
+        }
+
+        cout << "Current Title: " << title;
+        MovieReviews * current = head;
+        int count = 1;
+        while (current) {
+            cout << "Review #" << count++ << ": " << current->rating << ": " << current->comment << endl;
+            current = current->next;
+        }
+    }
+};
 
 int main() {
     //File Opening
@@ -32,88 +60,30 @@ int main() {
 	    return 1;
 	}
 
-    vector<Movie> movieList;
+    vector<Movie*> movieList;
 
     //Movie populating
-    MovieReviews * head = nullptr;
-    MovieReviews * current = nullptr;
     Movie * NewMovie = nullptr;
 
     string line;
-    string title;
     int rating;
     bool nextMovie = true;
     
     while (getline(file, line)) {
         if (nextMovie) { //First line / or any new movie title
-            title = line;
-            rating = rand() % 100;
+            NewMovie = new Movie(line);
             nextMovie = false;
         }  
-        else if (line == "") { //Means to move to the next movie, populates current movie
+        else if (line == "") { //Means to move to the next movie, adds the movie to the list
+            movieList.push_back(NewMovie);
             nextMovie = true;
         }
         else { //Is a review, populates review
-            if (!head) {
-                current->comment = line;
-                head = 
-            }
+            
+            rating = (rand() % 5) + 1;
+            NewMovie->addReview(rating, line);
         }
     }
 
-    MovieReviews * head = nullptr;
-
-    //Asks for User input on each movie
-    char continueKey = 'Y';
-    while (continueKey == 'Y') { 
-        MovieReviews * newVal = new MovieReviews;
-
-        cout << "Enter review rating 0-5: ";
-        cin >> newVal->rating;
-        while (newVal->rating > 5 || newVal->rating < 0) {
-            cout << "Enter a valid rating from 0-5: ";
-            cin >> newVal->rating;
-        }
-
-        cout << "Enter review comments: ";
-        cin >> newVal->comment;
-        cout << endl;
-
-        cout << "Enter another review? Y/N: ";
-        cin >> continueKey;
-        while (continueKey != 'Y' && continueKey != 'N' && continueKey != 'y' && continueKey != 'n') {
-            cout << "Enter a valid option Y/N: ";
-            cin >> continueKey;
-        }
-       
-        if (!head) { //if first
-            head = newVal;
-            newVal->next = nullptr;
-        }
-        else { //if second or more
-            newVal->next = head;
-            head = newVal;
-        }
- 
-    }
-
-    output(head);
-}
-
-void output(MovieReviews * head) {
-    if (!head) {
-        cout << "Empty list" << endl;
-        return;
-    }
-
-    MovieReviews * current = head;
-    int count = 1;
-    int sum = 0;
-    while (current) {
-        sum += current->rating;
-        cout << "Review #" << count++ << ": " << current->rating << ": " << current->comment << endl;
-        current = current->next;
-    }
-
-    cout << "Average: " << sum / (count - 1) << endl;
+    delete NewMovie;
 }
